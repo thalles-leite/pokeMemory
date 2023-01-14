38fd76adc7d684704696438cb086f262eb5416a1
Thalles Leite <eng.thallesleite@gmail.com>
2023-01-14 19:19:21 -0300

    Temporizador e efeitos sonoros adicionados.

--- script.js
+++ script.js
@@ -5,11 +5,45 @@ const cardBoard = document.querySelector('.cardBoard');
 const bCards = document.querySelector('.cards');
 const cards = document.querySelectorAll('.card');
 const blueLight = document.querySelector('.blueLight');
+let soundCorrect;
+let soundWrong;
+let soundVictory;
 
-// const gridAjust = () => {
-//   bCards.style.gridTemplateColumns = `repeat(${lineBoardCard}, 1fr)`;
-//   bCards.style.gridTemplateRows = `repeat(${lineBoardCard}, 1fr)`;
-// };
+// Cria um novo objeto do cronômetro
+const timer = new Timer();
+
+
+// Atualiza o tempo exibido na tela a cada segundo
+timer.addEventListener('secondsUpdated', () => {
+  document.getElementById('timer').innerHTML = `${timer.getTimeValues().toString(['minutes', 'seconds'])}`;
+});
+
+const loadSounds = () => {
+  soundCorrect = new Audio('./files/sounds/correct.wav');
+  soundWrong = new Audio('./files/sounds/wrong.wav');
+  soundVictory = new Audio('./files/sounds/win.wav');
+  soundCorrect.load();
+  soundWrong.load();
+  soundVictory.load();
+};
+
+const playSoundCorrect = () => {
+  soundCorrect.pause();
+  soundCorrect.currentTime = 0;
+  soundCorrect.play();
+};
+
+const playSoundWrong = () => {
+  soundWrong.pause();
+  soundWrong.currentTime = 0;
+  soundWrong.play();
+};
+
+const playSoundVictory = () => {
+  soundVictory.pause();
+  soundVictory.currentTime = 0;
+  soundVictory.play();
+};
 
 const arrayGenerator = (dificult) => {
   const arrayCards = [];
@@ -27,15 +61,29 @@ const arrayGenerator = (dificult) => {
 let firstClickedCard = '';
 let lastClickedCard = '';
 
+const victoryVerify = () => {
+  const corrects = document.querySelectorAll('.correct').length;
+  const cards = document.querySelectorAll('.card').length;
+  if (corrects >= cards) {
+    playSoundVictory();
+    timer.stop();
+    return true;
+  }
+};
+
 const correctMove = () => {
-  console.log('Certo!');
   firstClickedCard.previousSibling.classList.add('correct');
   lastClickedCard.previousSibling.classList.add('correct');
   firstClickedCard = '';
   lastClickedCard = '';
+  if (victoryVerify()) {
+    return;
+  }
+  playSoundCorrect();
 };
 
 const wrongMove = () => {
+  playSoundWrong();
   setTimeout(() => {
     firstClickedCard.parentNode.classList.remove('rotate');
     lastClickedCard.parentNode.classList.remove('rotate');
@@ -55,7 +103,7 @@ const checkPlay = (target) => {
   firstClickedCard.getAttribute('data-key') === lastClickedCard.getAttribute('data-key') ? correctMove() : wrongMove();
 };
 
-console.log(bCards.offsetWidth);
+
 const generateCard = (image, lados) => {
   const tamanhoCard = Math.round(bCards.offsetHeight / Math.sqrt(2 * lados)) * 0.76;
   const tamPercent = ((tamanhoCard / bCards.offsetWidth) * 100);
@@ -68,7 +116,11 @@ const generateCard = (image, lados) => {
   card.style.height = `${tamPercent}%`;
 
   card.addEventListener('click', ({ target }) => {
-    checkPlay(target);
+    if (firstClickedCard === '' || lastClickedCard === '') {
+      // Inicia o cronômetro
+      timer.start({ precision: 'secondTenths' });
+      checkPlay(target);
+    }
   });
 
   frontCard.className = 'frontCard side';
@@ -88,9 +140,13 @@ const generateAllCards = (lados) => {
     generateCard(element, lados);
   });
 };
-let cont = 1;
+let cont = 2;
 
 blueLight.addEventListener('click', () => {
+  timer.stop();
+  timer.reset();
+  firstClickedCard = '';
+  lastClickedCard = '';
   generateAllCards(cont);
   console.log(cont);
   cont += 1;
@@ -102,6 +158,8 @@ blueLight.addEventListener('contextmenu', (element) => {
   cont -= 1;
 });
 
+
+loadSounds();
 
 // gridAjust();
 
